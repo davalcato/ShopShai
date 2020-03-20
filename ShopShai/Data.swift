21a3f8f148437df0d21868745af92574179a1f47
davalcato <davalcato@yahoo.com>
2020-03-19 18:55:16 -0700

    added the Image(drink.imageName) to the DrinkItem file

--- ShopShai/Data.swift
+++ ShopShai/Data.swift
@@ -10,10 +10,10 @@ import Foundation
 
 let drinkData:[Drink] = load("drinks.json")
 
-
 func load<T:Decodable>(_ filename:String, as type:T.Type = T.self) -> T {
     let data:Data
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
         fatalError("Couldn't find \(filename) in main bundle.")
     }
     
@@ -27,7 +27,7 @@ func load<T:Decodable>(_ filename:String, as type:T.Type = T.self) -> T {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
     } catch {
-        fatalError("Couldn't parse \(filename) in main bundle:\n\(error)")
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
 
--- ShopShai/DrinkItem.swift
+++ ShopShai/DrinkItem.swift
@@ -8,14 +8,18 @@
 
 import SwiftUI
 
+
 struct DrinkItem: View {
+    
+    var drink:Drink
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(drink.imageName)
     }
 }
 
 struct DrinkItem_Previews: PreviewProvider {
     static var previews: some View {
-        DrinkItem()
+        DrinkItem(drink: drinkData[0])
     }
 }
